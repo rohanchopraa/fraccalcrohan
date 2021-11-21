7c0807d909a401f71b1699ccc2d98e8f01aca0da
rohanchopraa <89223414+rohanchopraa@users.noreply.github.com>
2021-11-21 11:28:33 -0800

    Add files via upload

---
+++ fraccalc21-22-rohan-chopra copy/FracCalc.java
@@ -0,0 +1,64 @@
+import java.util.*; 
+
+/**
+ * Calculates fractions.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class FracCalc {
+
+    /**
+     * Runs a fraction calculator.
+     * 
+     * @param args Not used
+     */
+    public static void main(String[] args) 
+    {
+        Scanner inputScanner = new Scanner(System.in);
+        String inputLine = inputScanner.nextLine();
+        System.out.println(produceAnswer(inputLine));
+    }
+
+    /**
+     * ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.
+     * This function will be used to test your code
+     * This function takes a String 'input' and produces the result
+     * 
+     * @param input A fraction string that needs to be evaluated.
+     * For your program, this will be the user input.
+     *     e.g. input ==> "1/2 + 3/4"
+     * @return The result of the fraction after it has been calculated
+     *     e.g. return ==> "1_1/4"
+     */
+    public static String produceAnswer(String input)
+    { 
+        Scanner frac = new Scanner(System.in); //scanner that gets a user input which i can return
+        String input1 = frac.next(); //the .next basically gets the next complete token and i can return it
+        String operator = frac.next(); //it gets the operand input since its after the first fraction/number
+        String input2 = frac.next(); //it gets the second fraction/number
+        return input1;
+        
+        //return "";
+    }
+
+    // TODO: Fill in the space below with any helper methods
+
+    /**
+     * GCF
+     * 
+     * @param p first int
+     * @param q second int
+     * @return The GCF
+     */
+    public static int gcf(int p, int q) {
+        while (q != 0) {
+            int temp = q;
+            q = p % q;
+            p = temp;
+        }
+        return p;
+    }
+
+}
+
